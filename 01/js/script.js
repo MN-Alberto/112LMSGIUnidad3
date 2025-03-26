4afc0374609fbf125322ab988d96c1f6046af762
Alberto <alberto.mennun@educa.jcyl.es>
2025-03-26 12:30:07 +0100

    01

--- 01/js/script.js
+++ 01/js/script.js
@@ -11,17 +11,28 @@ cuerpo.style.backgroundColor="#f4f4f4";
 cuerpo.style.textAlign="center";
 
 titulo.style.color="#333";
-titulo.style.fontStyle="forte";
+titulo.style.fontWeight="bold";
 titulo.style.marginBottom="15px";
-titulo.style.text-Transform("uppercase");
+titulo.style.textTransform="uppercase";
 
-parrafos.style.fontSize="18px";
-parrafos.style.padding="10px";
-parrafos.style.withd="60%";
-parrafos.style.borderleft="5px solid";
-parrafos.style.borderradio="5px";
-parrafos.style.lineHeight="1.6";
+for (var i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.fontSize = "18px";
+    parrafos[i].style.padding = "10px";
+    parrafos[i].style.width = "60%";
+    parrafos[i].style.borderLeft = "5px solid";
+    parrafos[i].style.borderRadius = "5px";
+    parrafos[i].style.lineHeight = "1.6";
+    parrafos[i].style.margin = "0 auto 15px";
+}
 
 parrafos[0].style.color="#e74c3c";
 parrafos[0].style.backgroundColor="#ffe6e6";
-parrafos[0]
+parrafos[0].style.borderColor="#c0392b";
+
+parrafos[1].style.color="#2ecc71";
+parrafos[1].style.backgroundColor="#eaffea";
+parrafos[1].style.bordercolor="#27ea60";
+
+parrafos[2].style.color="#3498db";
+parrafos[2].style.backgroundColor="#e6f2ff";
+parrafos[2].style.bordercolor="#2980b9";
